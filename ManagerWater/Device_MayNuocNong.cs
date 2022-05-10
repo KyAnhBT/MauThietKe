57cd46ae24e18b875eb921905847b1ac2d33b561
phamngocphung1 <thezootree@gmail.com>
2022-05-10 09:23:31 +0700

    add command pattern vao bai cuoi ky

---
+++ ManagerWater/Device_MayNuocNong.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ManagerWater
+{
+    //AP Factory Pattern
+    //SubClass
+    public class Device_MayNuocNong : Device
+    {
+        public Device_MayNuocNong(int cubicMetreID) : base("MayNuocNong", 0.14f, cubicMetreID)
+        {
+
+        }
+
+        public override void turnOff()
+        {
+            this.stop();
+        }
+
+        public override void turnOn()
+        {
+            this.use();
+        }
+    }
+}
---
+++ ManagerWater/ICommand_MayNuocNong.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagerWater
+{
+    //Ap command pattern
+    //ConcreteCommand
+    public class ICommand_MayNuocNong: ICommand
+    {
+        private Device_MayNuocNong mayNuocNong;
+
+        public ICommand_MayNuocNong(Device_MayNuocNong mayNuocNong)
+        {
+            this.mayNuocNong = mayNuocNong;
+        }
+
+        public string getName()
+        {
+            return "MayNuocNong";
+        }
+
+        public void turnOff()
+        {
+            mayNuocNong.turnOff();
+        }
+
+        public void turnOn()
+        {
+            mayNuocNong.turnOn();
+        }
+    }
+}
